ad47d37196768204d041eaeb4c8ad2d4512031ed
Nikita <river600@rambler.ru>
2023-02-13 17:51:18 +0300

    Homework for 2.12

--- src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
@@ -1,6 +1,7 @@
 package pro.sky.calculator.service;
 
 import org.springframework.stereotype.Service;
+import pro.sky.calculator.exception.DivisionByZeroException;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService{
@@ -25,7 +26,7 @@ public class CalculatorServiceImpl implements CalculatorService{
     @Override
     public String showDivisionOperation(int number1, int number2) {
         if(number2 == 0) {
-            return "Please, enter another second number, zero is illegal argument";
+            throw new DivisionByZeroException("The wrong number. Please, enter another second number, zero is illegal argument");
         }
         int result = number1 / number2;
         return number1 + " / " + number2 + " = " + result;
